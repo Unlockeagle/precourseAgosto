cb8e3f20c516620d179aa8c5bead4049f512b80d
Yoan <135377548+Unlockeagle@users.noreply.github.com>
2023-08-05 23:52:40 -0400

    mi segundo commit

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,34 +4,51 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string
 }
+devolverString('hola')
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var sumar = x + y
+   return sumar
 }
+suma(4, 6)
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var restar = x - y;
+   return restar;
 }
+resta(5, 3)
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var div = x / y;
+   return div;
 }
+divide(10, 2);
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var mult = x * y;
+   return mult;
 }
+multiplica(2, 5)
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var resto = x % y;
+   return resto;
 }
+obtenerResto(21, 5)
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,33 +5,43 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   return x === y
 }
+
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   return str1.length === str2.length
 }
 
+   
+
+
 function menosQueNoventa(num) {
+   return num < 90;
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
 }
 
 function mayorQueCincuenta(num) {
+   return num > 50;
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
 }
 
 function esPar(num) {
-   // Retorna true si "num" es par.
-   // De lo contrario, retorna false.
-   // Tu código:
+   return num % 2 === 0;
 }
+// Retorna true si "num" es par.
+// De lo contrario, retorna false.
+// Tu código:
 
 function esImpar(num) {
+   return num % 3 != 0
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
